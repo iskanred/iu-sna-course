fc2ad434b77bce8199a6a45bdf1851e20a14f583
Iskander <37081728+Iskanred@users.noreply.github.com>
2022-10-16 18:50:27 +0300

    Update ex7.sh

--- week07/ex7.sh
+++ week07/ex7.sh
@@ -1,1 +1,13 @@
+while :
+do
+	mem=`free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }'`
+	disk=`df -h | awk '$NF=="/"{printf "%s\t\t", $5}'`
+	cpu=`top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}'`
+	
+	output="$(date) memory usage = $mem\ndisk usage = $disk\ncpu usage=$cpu"
+  printf $output
+	printf $output >> /var/log/system_utilization.log
+  
+	sleep 15
+done
 
